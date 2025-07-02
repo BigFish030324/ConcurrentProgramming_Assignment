1a48546924545a6d202142fc9345de27f040622c
BigFish <130629379+BigFish030324@users.noreply.github.com>
2025-07-02 14:09:35 +0800

    added comment and 2 private define
    - Added comments from assignment questions
    - Import 2 java util library for customize and concurrent actions
    - Added 2 private define which is total order and random

--- OrderIntakeSystem.java
+++ OrderIntakeSystem.java
@@ -1,3 +1,12 @@
+// Order Intake System
+// Task:
+// - Order arrive from the online platform at a rate of 1 order every 500ms
+// - Each order is verified for payment, inventory availability and shipping address
+
+import java.util.Random;
+import java.util.concurrent.*;
+
 public class OrderIntakeSystem {
-    
+    private int totalOrder;
+    private Random random;
 }
