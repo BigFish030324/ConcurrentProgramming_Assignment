19570fb257e3a425dff43b9ad4cc6260d0f79a45
BigFish <junhongeu030324@gmail.com>
2025-07-03 23:54:35 +0800

    Updated comments in Packing Station
    Added comments on what to do next in this file

--- PackingStation.java
+++ PackingStation.java
@@ -1,3 +1,8 @@
+// Packing Station
+// Task:
+// - Completed bins are packed into shipping boxes (1 order at a time)
+// - A scanner checks each box to ensure contents match the order
+
 public class PackingStation {
     
 }
