e3d10d80fc0e4b92c6dc209e4ca3ece552faba26
BigFish <junhongeu030324@gmail.com>
2025-07-03 23:43:09 +0800

    Created BackEnd file
    Previously is called Statistics but changed name to BackEnd:
    - Comment To-Do tasks
    - Comment notes what is missing in other sections file

---
+++ BackEnd.java
@@ -0,0 +1,9 @@
+// Collects global counters and signals when processing is complete
+// To-Do: After all 6 basic sections are done, remember to implement the backends
+
+public class BackEnd {
+    // OrderIntakeSystem - Code when reject, reject count and done processed count both +1
+    
+    // PickingStation - when all works are done, break loop
+    // Picking Station - Code when reject, reject count and done processed count both +1
+}
