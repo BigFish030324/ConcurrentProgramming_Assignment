fe58b502c1196fb0e0fa6dff46fb8ff4f3a8f3bd
BigFish <junhongeu030324@gmail.com>
2025-07-04 11:14:31 +0800

    Update BackEnd
    - Added spacing for order intake system and picking system
    - Added new comment for packing station

--- BackEnd.java
+++ BackEnd.java
@@ -2,8 +2,11 @@
 // To-Do: After all 6 basic sections are done, remember to implement the backends
 
 public class BackEnd {
-    // OrderIntakeSystem - Code when reject, reject count and done processed count both +1
+    // Order Intake System - Code when reject, reject count and done processed count both +1
     
-    // PickingStation - when all works are done, break loop
+    // Picking Station - when all works are done, break loop
     // Picking Station - Code when reject, reject count and done processed count both +1
+
+    // Packing Station - when all works are done, break loop
+    // Packing Station - Code for box count and done processed count both +1
 }
