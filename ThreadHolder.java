bdd9b84c3d4625a98a90dea118f644c833dc8e61
BigFish <junhongeu030324@gmail.com>
2025-07-20 17:11:58 +0800

    Added content for Packing Station
    - Added content in Packing Station and Thread Holder java files

--- ThreadHolder.java
+++ ThreadHolder.java
@@ -2,4 +2,17 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ThreadHolder {
+    // Queues
+    public static BlockingQueue<Order> packingQueue = new LinkedBlockingQueue<>();
+    public static BlockingQueue<Box> labellingQueue = new LinkedBlockingQueue<>();
+    
+    // Counters for statistics
+    public static volatile int totalOrdersPacked = 0;
+    
+    // If thread is completed
+    public static volatile boolean pickingFinished = false;
+    public static volatile boolean packingFinished = false;
+    
+    // Thread Names
+    public static final String PACKER_1 = "Packer-1";
 }
