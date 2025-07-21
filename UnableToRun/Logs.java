ab6aeb2de07674a1bd785330db3fb36bcc7314af
BigFish <130629379+BigFish030324@users.noreply.github.com>
2025-07-21 16:40:53 +0800

    Delete Unable to Run folder
    - delete the folder that contains previous codes

--- UnableToRun/Logs.java
+++
@@ -1,9 +0,0 @@
-package UnableToRun;
-// Centralized logging utility class
-
-public class Logs {
-    // Implement `synchronized` to ensure ONLY one thread at a time
-    public static synchronized void log(String message) {
-        System.out.println(message);
-    }
-}
